232aa8afd2685f2454d91c7663f26fc96674ee65
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2025-04-20 17:53:09 +0500

    Refactor CustomRegisterView to handle name extraction from email without modifying request.data directly

--- backend/kafka_backend/useraccounts/views.py
+++ backend/kafka_backend/useraccounts/views.py
@@ -6,14 +6,26 @@ from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.response import Response
 
+from django.shortcuts import render  # noqa
+# Create your views here.
+from dj_rest_auth.registration.views import RegisterView
+from django.db import IntegrityError
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.request import Request
+
 class CustomRegisterView(RegisterView):
     def create(self, request, *args, **kwargs):
         try:
             # If name is not provided, extract it from the email
             if not request.data.get('name') and request.data.get('email'):
-                request.data._mutable = True
-                request.data['name'] = request.data['email'].split('@')[0]
-                request.data._mutable = False
+                email = request.data.get('email')
+                name = email.split('@')[0]
+
+                # Instead of modifying request.data directly, create a modified copy
+                # and override the get_serializer method to use our modified data
+                self.modified_data = {**request.data}
+                self.modified_data['name'] = name
 
             return super().create(request, *args, **kwargs)
         except IntegrityError:
@@ -21,4 +33,11 @@ class CustomRegisterView(RegisterView):
                 {"email": ["This email is already registered."]},
                 status=status.HTTP_400_BAD_REQUEST
             )
- # changes made with this file
+
+    def get_serializer(self, *args, **kwargs):
+        # If we have modified data, use it for the serializer
+        if hasattr(self, 'modified_data'):
+            kwargs['data'] = self.modified_data
+        return super().get_serializer(*args, **kwargs)
+
+# Change the regiteration view
